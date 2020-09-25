3f78c4e5feef677b8da5d75330a30213e575456b
Phuong Le <lehoangbro@gmail.com>
2020-09-25 10:57:13 +0700

    1st commit

---
+++ math/calculation.py
@@ -0,0 +1,18 @@
+def total(a,b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiple(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print("Test thu cac phep tinh")
+    print("1 cong 1 bang",total(1, 1))
+    print("15 tru 5 bang",subtract(15, 5))
+    print("123 nhan 321 bang", multiple(123, 321))
+    print("10000 chia 25 bang", divide(10000, 25))
